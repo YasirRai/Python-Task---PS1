363685d09dd614cab55124dbc81c1a7f3973a1f8
Yasir <99413125+YasirRai@users.noreply.github.com>
2022-02-27 22:10:31 +0500

    Add files via upload

---
+++ 4th Task.py
@@ -0,0 +1,44 @@
+# first task 10 customers data, height is mandatory in decimals and contact no in numbers
+
+c1 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c2 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c3 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c4 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c5 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c6 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c7 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c8 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c9 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+c10 = (input('what is your name ')), float(input('what is your height ')), int(input('write your contact number '))
+print(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10)
+
+# 2nd task Skillvention Admission form
+print('for office Use only'.center(30))
+C = input('course ')
+b = input('batch' )
+r = input('registration ')
+print('Filled by user'.center(30))
+n = input('name ')
+fn = input('father name ')
+DOB = input('date of birth ')
+pr = input('permanent address ')
+r1 = input('religion ')
+co = input('course ')
+no = ('contact ')
+ECN = ('emergency contact no ')
+em = (' email ')
+g = ('geneder ')
+q = ('qualification ')
+sk = ('skillset ')
+ms = (' martial status ')
+fpm = ('fee payment mood' )
+am = ('agreement'.center(10))
+
+# task 3 login in new registration form
+
+first_Name = input('what is your first name ')
+last_name = input('what is your last name ')
+email = input('write your email address ')
+pswd = input('type your password ')
+pswd1 = input('retype your password ')
+
