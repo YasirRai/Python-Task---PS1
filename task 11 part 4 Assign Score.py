67dccfbd22b1efadbe57de77dc120930647e33e6
Yasir <99413125+YasirRai@users.noreply.github.com>
2022-04-04 14:12:08 +0500

    Delete task 11 part 4 Assign Score.py

--- task 11 part 4 Assign Score.py
+++
@@ -1,6 +0,0 @@
-import random
-random.seed(1)
-names = ['Abdul Muhaimin', 'Ali', 'Hammad', 'farhan', 'Rehan', 'Nazal', 'yasir']
-for x in names:
-    score = random.randint(60, 90)
-    print(x, "your score is", score)
