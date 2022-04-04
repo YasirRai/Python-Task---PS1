158b1aaeb4bf209faf14f96746a7f139ddf6b525
Yasir <99413125+YasirRai@users.noreply.github.com>
2022-04-04 14:10:52 +0500

    Delete Task 10 part 7 area of circle.py

--- Task 10 part 7 area of circle.py
+++
@@ -1,9 +0,0 @@
-# Define a function that accepts radius and returns the area of a circle.
-
-def area():
-    ridus = int(input("Enter the Ridus of Circle "))
-    pi =3.14
-    a = pi*(ridus**2)
-    print(f'Area of the cirle is: {a}')
-
-area()
